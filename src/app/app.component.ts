a9f8a58561ad3ccb5517d8d57ffbe0952e1a5d75
Stephen Mitchell <stephen@tripbutty.com>
2021-09-07 11:44:59 +0200

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: '[app-root]',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'twosteps';
+
+  constructor (
+    public updates: SwUpdate,
+  ) {
+    updates.available.subscribe(event => {
+      if (window.confirm("A new version of this app is available! Download the new version?")) {
+        window.location.reload();
+      }
+    });
+  }
 }
