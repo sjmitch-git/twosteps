6d1a21e949d54d1cb511cf1549fc1e668bd332ef
sjmitch-git <sjmitch20@outlook.com>
2022-10-06 15:43:46 +0100

    gtag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { SwUpdate } from '@angular/service-worker';
+import  { Router, NavigationEnd } from "@angular/router";
+declare let gtag: any;
 
 @Component({
   selector: '[app-root]',
@@ -11,7 +13,14 @@ export class AppComponent {
 
   constructor (
     public updates: SwUpdate,
+    private router: Router
   ) {
+    this.router.events.subscribe(value => {
+      if(value instanceof NavigationEnd) {
+        gtag('config', 'UA-UA-125394580-9', {'page_path':this.router.url.toString()});
+      }    
+    });
+
     updates.available.subscribe(event => {
       if (window.confirm("A new version of this app is available! Download the new version?")) {
         window.location.reload();
