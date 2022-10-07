250abf6cd3259c845e21b2c36b24f78f54e0a0e2
sjmitch-git <sjmitch20@outlook.com>
2022-10-07 13:07:08 +0100

    sitemap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 import { SwUpdate } from '@angular/service-worker';
-// import  { Router, NavigationEnd } from "@angular/router";
-// import { filter } from 'rxjs/operators';
 
 declare let gtag: any;
 
@@ -12,30 +10,10 @@ declare let gtag: any;
 })
 export class AppComponent {
   title = 'twosteps';
- // routeChange;
 
   constructor (
-    public updates: SwUpdate,
-   // private router: Router
+    public updates: SwUpdate
   ) {
-
-    /* this.routeChange = router.events
-    .pipe(
-      filter((event): event is NavigationEnd => event instanceof NavigationEnd)
-    )
-    .subscribe(e => {
-      setTimeout(() => {
-        gtag('config', 'UA-125394580-9', {'page_path':e.urlAfterRedirects});
-      }, 4000);
-    }); */
-
-   /*  this.router.events.subscribe(value => {
-      if(value instanceof NavigationEnd) {
-        console.log('page_path', this.router.url.toString())
-        gtag('config', 'UA-UA-125394580-9', {'page_path':this.router.url.toString()});
-      }    
-    }); */
-
     updates.available.subscribe(event => {
       if (window.confirm("A new version of this app is available! Download the new version?")) {
         window.location.reload();
